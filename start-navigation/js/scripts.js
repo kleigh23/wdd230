7e2d897ba2ef5587a61e7972070a376f8d5e764d
kleigh23 <rob22003@byui.edu>
2022-05-10 13:09:49 -0600

    finished

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+
+const x = document.querySelector('#hamburgerBtn')
+x.onclick = toggleMenu;
