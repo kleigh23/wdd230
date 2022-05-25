e6720eb8083c11c1ddc4d6eb828765836c52b1f9
kleigh23 <rob22003@byui.edu>
2022-05-25 17:51:03 -0600

    updated for the wind chill

--- chamber/js/current_date.js
+++ chamber/js/current_date.js
@@ -29,7 +29,7 @@ let year = now.getFullYear();
 document.querySelector("#year").textContent = year;
 
 const dayNumber = now.getDay();
-console.log(dayNumber);
+//console.log(dayNumber);
 
 const element = document.querySelector("#message");
 
@@ -37,4 +37,22 @@ if (dayNumber == 1 || dayNumber == 2) {
     element.classList.add("showme");
 } else {
     element.classList.add("hideme");
+}
+
+// this is for the wind chill
+const tempNumber = parseFloat(document.querySelector("#tempeture").textContent);
+//console.log(tempNumber);
+
+const speedNumber = parseFloat(document.querySelector("#speed").textContent);
+//console.log(speedNumber);
+
+let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber,
+    0.16)) + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
+windchill = Math.round(windchill);
+//console.log(windchill);
+
+if(tempNumber<=50 && speedNumber >3) {
+    document.querySelector("#chill").textContent = "Wind Chill: "+windchill+"\xB0F";
+} else {
+    document.querySelector("#chill").textContent = "Wind Chill: N/A"
 }
