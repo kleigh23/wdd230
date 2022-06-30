6e72ea1fd22ca52d1e4e0f3872a22826a1df03dd
kleigh23 <rob22003@byui.edu>
2022-06-30 12:56:44 -0600

    fixed some errors my peers saw

---
+++ chamber/js/join.js
@@ -0,0 +1,64 @@
+// this is for the current date in the header.
+const datefieldUK = document.querySelector("#currentDate");
+
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+// this is for the hamburger button.
+function toggleMenu() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.querySelector('#hamburgerBtn')
+x.onclick = toggleMenu;
+
+
+// this is for the last modfidied date in the footer.
+let oLastModif = document.lastModified;
+document.querySelector("#currentdate").textContent = oLastModif;
+
+
+// This is for the year for the copy right.
+let year = now.getFullYear();
+document.querySelector("#year").textContent = year;
+
+// lazy load js
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+const intersectionCallback = (items, observer) => {
+    items.forEach((item) => {
+        if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target)
+        }
+    });
+}
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(intersectionCallback, imgOptions); 
+    imagesToLoad.forEach((img) => {
+        observer.observe(img)
+    });
+}
+
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
